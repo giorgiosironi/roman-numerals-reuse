503796d2ba7fbed6665c99dad8ef243e688a913f
Giorgio Sironi <giorgio.sironi@neomobile.com>
2013-12-08 15:42:13 +0100

    Indentation for SubtractiveRule

--- SubtractiveRule.php
+++ SubtractiveRule.php
@@ -5,13 +5,12 @@ class SubtractiveRule
     public function representationFor($representation, $repetitions, $symbol, $lastSymbol, $nextToLastSymbol)
     {
         if ($repetitions > 3) {
-
-        if (end($representation) == $lastSymbol) {
-            array_pop($representation);
-            $representation[] = $symbol . $nextToLastSymbol;
-        } else {
-            $representation[] = $symbol . $lastSymbol;
-        }
+            if (end($representation) == $lastSymbol) {
+                array_pop($representation);
+                $representation[] = $symbol . $nextToLastSymbol;
+            } else {
+                $representation[] = $symbol . $lastSymbol;
+            }
         } 
         return $representation;
     }
