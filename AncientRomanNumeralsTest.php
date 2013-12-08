c0474a6b11fac664995f820bc9c878a511351f81
Giorgio Sironi <giorgio.sironi@neomobile.com>
2013-12-08 15:13:48 +0100

    Separating ancient from medieval Roman numerals requires configurability of the subtractive rule

---
+++ AncientRomanNumeralsTest.php
@@ -0,0 +1,22 @@
+<?php
+
+class AncientRomanNumeralsTest extends PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        $this->system = new AddictiveAndSubtractiveNumeralSystem(
+            $symbols = [
+                5 => new Symbol('V'),
+                1 => new Symbol('I'),
+            ]
+        );
+    }
+
+    public function testSymbolsCanBeRepeatesMoreThan3Times()
+    {
+        $this->markTestIncomplete();
+        $this->assertEquals('IIII', $this->system->convert(1));
+    }
+
+    // testSymbolsCannotBeRepeatedIfThereAreLargerSymbolsThatCoverTheSameAmount
+}
