90cbc03ccedeed50a21cea5cd849bd2dff20741b
Giorgio Sironi <giorgio.sironi@neomobile.com>
2013-12-08 16:04:12 +0100

    Third characterization test for SubtractiveRule: no representation

--- SubtractiveRuleTest.php
+++ SubtractiveRuleTest.php
@@ -19,4 +19,13 @@ class SubtractiveRuleTest extends PHPUnit_Framework_TestCase
             $rule->representationFor(['V'], 4, 'I', 'V', 'X')
         );
     }
+
+    public function testDoesNotProduceARepresentationIfSubtractiveRepresentationIsNotNeeded()
+    {
+        $rule = new SubtractiveRule(3);
+        $this->assertEquals(
+            ['V'],
+            $rule->representationFor(['V'], 2, null, null, null)
+        );
+    }
 }
