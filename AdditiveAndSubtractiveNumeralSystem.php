3a78203f5913e067d0e0f744a6e45ac01f7b7396
Giorgio Sironi <giorgio.sironi@neomobile.com>
2013-12-08 15:41:23 +0100

    Iterating over $this->rules

--- AdditiveAndSubtractiveNumeralSystem.php
+++ AdditiveAndSubtractiveNumeralSystem.php
@@ -18,8 +18,13 @@ class AdditiveAndSubtractiveNumeralSystem
         $nextToLastSymbol = null;
         foreach ($this->symbols as $containedAmount => $symbol) {
             $repetitions = floor($number / $containedAmount);
+            foreach ($this->rules as $rule) {
+                $representation = $rule->representationFor($representation, $repetitions, $symbol, $lastSymbol, $nextToLastSymbol);
+            }
+                /*
                 $representation = $this->subtractiveRule->representationFor($representation, $repetitions, $symbol, $lastSymbol, $nextToLastSymbol);
                 $representation = $this->additiveRule->representationFor($representation, $repetitions, $symbol); 
+                 */
             $number -= $containedAmount * $repetitions;
             $nextToLastSymbol = $lastSymbol;
             $lastSymbol = $symbol;
