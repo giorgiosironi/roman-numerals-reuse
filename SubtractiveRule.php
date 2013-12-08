371c4cd8d4acced0fcf965ec8704bb475db6869c
Giorgio Sironi <giorgio.sironi@neomobile.com>
2013-12-08 16:23:07 +0100

    Starting to rename $...Symbol to [i]

--- SubtractiveRule.php
+++ SubtractiveRule.php
@@ -13,8 +13,13 @@ class SubtractiveRule
     {
         // TODO: complex code, unit test it
         // TODO: seems coupled to pieces of $representation
+        $arguments = func_get_args();
+        array_shift($arguments);
+        array_shift($arguments);
+        $largerSymbols = $arguments;
+        // $largerSymbols[0] not used
         if ($repetitions >= $this->lowerRepetitionsLimit) {
-            if (end($representation) == $lastSymbol) {
+            if (end($representation) == $largerSymbols[1]) {
                 array_pop($representation);
                 if (end($representation) == $secondLastSymbol . $thirdLastSymbol && $thirdLastSymbol) {
                     array_pop($representation);
